ae0c46aec6e46427b2efbc7abad17156b3d126cc
Hanno Schlichting <hanno@hannosch.eu>
2010-03-30 17:46:16 +0000

    Package boilerplate

---
+++ README.txt
@@ -0,0 +1,4 @@
+Overview
+========
+
+Record provides special objects used in some Zope2 internals like ZRDB.
---
+++ setup.py
@@ -0,0 +1,39 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from os.path import join
+from setuptools import setup, find_packages, Extension
+
+setup(name='Record',
+      version = '2.13.0dev',
+      url='http://pypi.python.org/pypi/Record',
+      license='ZPL 2.1',
+      description="Special Record objects used in Zope2.",
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=open('README.txt').read() + '\n' +
+                       open('CHANGES.txt').read(),
+
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      ext_modules=[Extension(
+            name='Record._Record',
+            include_dirs=['include', 'src'],
+            sources=[join('src', 'Record', '_Record.c')],
+            depends=[join('include', 'ExtensionClass', 'ExtensionClass.h')]),
+      ],
+      install_requires=['ExtensionClass'],
+      include_package_data=True,
+      zip_safe=False,
+      )
