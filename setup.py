2fcf5083ecf0274ffad779039384be63c25c3dd1
Hanno Schlichting <hanno@hannosch.eu>
2010-03-30 17:48:29 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from os.path import join
 from setuptools import setup, find_packages, Extension
 
 setup(name='Record',
-      version = '2.13.0',
+      version = '2.13.1dev',
       url='http://pypi.python.org/pypi/Record',
       license='ZPL 2.1',
       description="Special Record objects used in Zope2.",
