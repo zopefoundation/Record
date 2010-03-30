a9e46e2c81904d1b1f91450eb6554465306feb83
Hanno Schlichting <hanno@hannosch.eu>
2010-03-30 17:47:51 +0000

    Prepare Record 2.13.0.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from os.path import join
 from setuptools import setup, find_packages, Extension
 
 setup(name='Record',
-      version = '2.13.0dev',
+      version = '2.13.0',
       url='http://pypi.python.org/pypi/Record',
       license='ZPL 2.1',
       description="Special Record objects used in Zope2.",
