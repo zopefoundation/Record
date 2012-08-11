c8ec06922c444ca7673fb742f0396d5f4ae81b7d
Hanno Schlichting <hanno@hannosch.eu>
2012-08-11 15:51:52 +0000

    whitespace

--- setup.py
+++ setup.py
@@ -16,24 +16,23 @@ from os.path import join
 from setuptools import setup, find_packages, Extension
 
 setup(name='Record',
-      version = '2.13.1dev',
-      url='http://pypi.python.org/pypi/Record',
-      license='ZPL 2.1',
-      description="Special Record objects used in Zope2.",
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.org',
-      long_description=open('README.txt').read() + '\n' +
-                       open('CHANGES.txt').read(),
-
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      ext_modules=[Extension(
-            name='Record._Record',
-            include_dirs=['include', 'src'],
-            sources=[join('src', 'Record', '_Record.c')],
-            depends=[join('include', 'ExtensionClass', 'ExtensionClass.h')]),
-      ],
-      install_requires=['ExtensionClass'],
-      include_package_data=True,
-      zip_safe=False,
-      )
+    version='2.13.1dev',
+    url='http://pypi.python.org/pypi/Record',
+    license='ZPL 2.1',
+    description="Special Record objects used in Zope2.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=open('README.txt').read() + '\n' +
+        open('CHANGES.txt').read(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    ext_modules=[Extension(
+        name='Record._Record',
+        include_dirs=['include', 'src'],
+        sources=[join('src', 'Record', '_Record.c')],
+        depends=[join('include', 'ExtensionClass', 'ExtensionClass.h')]),
+    ],
+    install_requires=['ExtensionClass'],
+    include_package_data=True,
+    zip_safe=False,
+)
